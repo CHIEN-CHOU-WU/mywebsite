9c3252051ab3bb1b294776ba209f5ffd1c60a0f4
chienchouwu <johnson12440@gmail.com>
2020-10-17 20:06:20 -0500

    Update html, add postgresql model for stundetpredict

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -86,11 +86,17 @@ WSGI_APPLICATION = 'mywebsite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mywebsite',
+        'USER': 'postgres',
+        'PASSWORD': 'musicgood123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
--- studentpredict/views.py
+++ studentpredict/views.py
@@ -1,9 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from .models import studentperformance
 # Create your views here.
 def studentpredict_view(request, *args, **kwargs):
 	#context = {'object': studentperformance.objects.all()}
 	#return HttpResponse("<h1>Django 版本!<h1>")
+	obj = studentperformance.objects.all()
+	context = {
+		'object': obj
+	}
 
-	return render(request, "studentpredict/studentpredict.html", {})
+	return render(request, "studentpredict/studentpredict.html", context)
