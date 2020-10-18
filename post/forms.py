7c2dc6224b7dc10fa0798454c519753062404a49
chienchouwu <johnson12440@gmail.com>
2020-10-18 16:59:16 -0500

    Eddit homepage

--- post/forms.py
+++ post/forms.py
@@ -6,7 +6,7 @@ class PostForm(forms.ModelForm):
 		model = Post
 		fields = ('title', 'title_tag', 'author', 'body')
 		widgets = {
-			'title': forms.TextInput(attrs={'class': 'form-control'}),
+			'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Please enter your Title.'}),
 			'title_tag': forms.TextInput(attrs={'class': 'form-control'}),
 			'author': forms.Select(attrs={'class': 'form-control'}),
 			'body': forms.Textarea(attrs={'class': 'form-control'}),
